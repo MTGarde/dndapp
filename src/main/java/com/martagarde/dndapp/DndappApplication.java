e4170a3c5ca3e823e05ab118548f7c690c5d6a99
mgard <s22gardmart@venta.lv>
2026-01-30 15:28:06 +0200

    Initial commit

---
+++ src/main/java/com/martagarde/dndapp/DndappApplication.java
@@ -0,0 +1,13 @@
+package com.martagarde.dndapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martagarde/dndapp/DndappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martagarde.dndapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
